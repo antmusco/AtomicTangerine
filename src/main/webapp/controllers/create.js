e616f4a0dc5876a49b02c96ac2a19428c567de21
YuanyuanPeng <yuanyuan.peng@stonybrook.edu>
2016-04-27 21:57:28 -0400

    can add shapes to canvas but cannot change color

--- src/main/webapp/controllers/create.js
+++ src/main/webapp/controllers/create.js
@@ -35,16 +35,16 @@ app.controller('createCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.canvasOps = function canvasOps() {
 
 
-        var rect = new fabric.Rect({
-            left: 100,
-            top: 100,
-            fill: 'green',
-            width: 40,
-            height: 40,
-            angle: 0
-        });
-
-        $scope.canvas.add(rect);
+        // var rect = new fabric.Rect({
+        //     left: 100,
+        //     top: 100,
+        //     fill: 'green',
+        //     width: 40,
+        //     height: 40,
+        //     angle: 0
+        // });
+        //
+        // $scope.canvas.add(rect);
         $scope.canvas.renderAll();
     };
     
@@ -66,7 +66,35 @@ app.controller('createCtrl', ['$scope', '$http', function ($scope, $http) {
         $scope.pickedcolorstyle={color:color};
      
     };
+    
+    $scope.drawshape=function(shape){
+        if(shape=='cir'){
+            var circle = new fabric.Circle({
+                radius: 20, fill: 'yellow',
+                left:400, top:400
+            });
+            $scope.canvas.add(circle);
 
+        }else if(shape=='tri'){
+            var triangle = new fabric.Triangle({
+                width: 20, height: 30, fill: 'indigo',
+                left:200, top:200
+            });
+            $scope.canvas.add(triangle);
+
+        }else if(shape=='rect'){
+            var rectangle = new fabric.Rect({
+
+                fill: 'violet',
+                width: 20,
+                height: 40,
+                angle: 0,
+                left:300, top:300
+            });
+            $scope.canvas.add(rectangle);
+
+        };
+    };
 
     $scope.canvasInit = function () {
        console.log('bloop');
